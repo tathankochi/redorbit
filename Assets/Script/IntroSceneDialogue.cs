651fb6a28ebff3d45fef4a94258d7811c700c251
hypamutekie <nguyenkienmarbo0731@gmail.com>
2025-10-04 18:37:51 +0700

    Add dialogue UI and script to Intro Scene
    Introduced a Canvas with a DialogueBox and TextMeshPro text for displaying dialogue in the Intro Scene. Added the IntroSceneDialogue MonoBehaviour script to manage dialogue logic.

---
+++ Assets/Script/IntroSceneDialogue.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class IntroSceneDialogue : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public Dialog dialog;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
