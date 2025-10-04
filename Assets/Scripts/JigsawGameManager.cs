90e61f244d8bcdebdff8d11f21dda6960caac603
Suzune-pcz5 <fireflypcz5@gmail.com>
2025-10-04 14:28:19 +0700

    2

--- Assets/Scripts/JigsawGameManager.cs
+++ Assets/Scripts/JigsawGameManager.cs
@@ -1,21 +1,326 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class JigsawGameManager : MonoBehaviour {
+    [Header("Game Elements")]
+    [Range(2, 6)]
+    [SerializeField] private int diffculty = 4;
+    [SerializeField] private Transform gameHolder;
+    [SerializeField] private Transform piecePrefabs;
 
     [Header("UI Elements")]
     [SerializeField] private List<Texture2D> imageTextures;
     [SerializeField] private Transform levelSelectPanel;
     [SerializeField] private Image levelSelectPrefabs;
+    [SerializeField] private GameObject playAgainButton;
 
+    private List<Transform> pieces;
+    private Vector2Int dimensions;
+    private float width;
+    private float height;
+
+    private Transform draggingPiece = null;
+    private Vector3 offset;
+
+    private int piecesCorrect;
+    private int puzzleIndex;
+    public Dialog dialog;
     void Start(){
+        int index = 0;
         //Create the UI
         foreach (Texture2D texture in imageTextures) {
             Image image = Instantiate(levelSelectPrefabs, levelSelectPanel);
             image.sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), Vector2.zero);
+            //Assign button action
 
+            //Fix closure bug; create copy
+            int capturedIndex = index;
+            Texture2D capturedTexture = texture;
+
+            image.GetComponent<Button>().onClick.AddListener(delegate { StartGame(capturedTexture, capturedIndex); });
+            Debug.Log(">>>> index gan " + capturedIndex);   
+            index++;
+        }
+        dialog.ResetDialog(); 
+        dialog.gameObject.SetActive(true);
+        dialog.dialogLines = new string[] {
+        "You found the jigsaw puzzle!",
+            "Putting together puzzles is a fun way to relax.",
+            "Press SPACE to continue..."
+        };
+        dialog.StartDialog();
+    }
+
+    public void StartGame(Texture2D jigsawTexture, int index) {
+        puzzleIndex = index;
+        Debug.Log(">>> index hien tai" + puzzleIndex);
+        //Hide the UI
+        levelSelectPanel.gameObject.SetActive(false);
+
+        //store a list of the transform fo each jigsaw piece so we can track then later
+        pieces = new List<Transform>();
+
+        //Calculate the size of each jigsaw piece, based on a difficulty setting
+        dimensions = GetDimensions(jigsawTexture, diffculty);
+
+        //Create the pieces of the correct size with the correct texture
+        CreateJigsawPieces(jigsawTexture);
+
+        //Place the pieces randomly into the visible area
+        Scatter();
+
+        //Update the border to fit the chosen puzzle
+        UpdateBorder();
+
+        //At start, there will be no correct pieces
+        piecesCorrect = 0;
+    }
+    Vector2Int GetDimensions(Texture2D jigsawTexture, int difficulty) { 
+        Vector2Int dimensions = Vector2Int.zero;
+        //Diffculty is the number of pieces on the smallest texture dimension.
+        //This helps ensure the pieces are as square as possible.
+        if(jigsawTexture.width < jigsawTexture.height){
+            dimensions.x = difficulty;
+            dimensions.y = (diffculty * jigsawTexture.height) / jigsawTexture.width;
+        } else {
+            dimensions.x = (difficulty * jigsawTexture.width) / jigsawTexture.height;
+            dimensions.y = difficulty;
+        }
+        return dimensions;
+    }
+    //Create all the jigsaw pieces 
+    void CreateJigsawPieces(Texture2D jigsawTexture) {
+        //Calulate piece sizes based on the dimensions
+        height = 1f / dimensions.y;
+        float aspect = (float)jigsawTexture.width / jigsawTexture.height;
+        width = aspect / dimensions.x;
+
+        for (int row = 0; row < dimensions.y; row++) {
+            for (int col = 0; col < dimensions.x; col++) {
+                //Create the piece in the right location of the right size
+                Transform piece = Instantiate(piecePrefabs, gameHolder);
+                piece.localPosition = new Vector3(
+                    (-width * dimensions.x / 2) + (width * col) + (width / 2),
+                    (-height * dimensions.y / 2) + (height * row) + (height / 2),
+                    -1);
+                //don't have to name them
+                piece.name = $"Piece {(row * dimensions.x) + col}";
+                pieces.Add(piece);
+
+                //Assign the correct part of the texture for this jigsaw piece
+                //We need our width and height both to be normalized between 0  and 1 for the UV
+                float width1 = 1f / dimensions.x;
+                float height1 = 1f / dimensions.y;
+                //UV coord order is anti-clockwwise : (0, 0), (1, 0), (0, 1), (1, 1)
+                Vector2[] uv = new Vector2[4];
+                uv[0] = new Vector2(width1 * col, height1 * row);
+                uv[1] = new Vector2(width1 * (col + 1), height1 * row);
+                uv[2] = new Vector2(width1 * col, height * (row + 1));
+                uv[3] = new Vector2(width1 * (col + 1), height * (row + 1));
+                //Assign our new  UVs to the mesh
+                Mesh mesh = piece.GetComponent<MeshFilter>().mesh;
+                mesh.uv = uv;
+                //Update the texture on the piece
+                piece.GetComponent<MeshRenderer>().material.SetTexture("_MainTex", jigsawTexture);
+            }
         }
     }
+
+
+        //Place the pieces randomly in the visible area
+        private void Scatter() {
+            //Calculate the visible orthographic size of the screen
+            float orthoHeight = Camera.main.orthographicSize;   
+            float screenAspect = (float)Screen.width / Screen.height;   
+            float orthoWidth = (screenAspect * orthoHeight);
+
+            //Ensure pieces are away from the edges
+            float pieceWidth = width * gameHolder.localScale.x;
+            float pieceHeight = height * gameHolder.localScale.y;
+
+            orthoHeight -= pieceHeight;
+            orthoWidth -= pieceWidth;
+
+            //Place each piece randomly in the visible area
+            foreach (Transform piece in pieces){
+                float x = Random.Range(-orthoWidth, orthoWidth);
+                float y = Random.Range(-orthoHeight, orthoHeight);
+                piece.position = new Vector3(x, y, -1);
+            }
+        }
+    //Update the border to fit the chosen puzzle
+    private void UpdateBorder() {
+        LineRenderer lineRenderer = gameHolder.GetComponent<LineRenderer>();
+
+        //Calculate half sizes to simplify the code
+        float halfWidth = (width * dimensions.x) / 2f;
+        float halfHeight = (height * dimensions.y) / 2f;
+
+        //border to be behind the pieces
+        float borderZ = 0f;
+
+        //Set border vertices, staring top left, going clockwise
+        lineRenderer.SetPosition(0, new Vector3(-halfWidth, halfHeight, borderZ));
+        lineRenderer.SetPosition(1, new Vector3(halfWidth, halfHeight, borderZ));
+        lineRenderer.SetPosition(2, new Vector3(halfWidth, -halfHeight, borderZ));
+        lineRenderer.SetPosition(3, new Vector3(-halfWidth, -halfHeight, borderZ));
+
+        //Set the thickness of the border line
+        lineRenderer.startWidth = 0.1f;
+        lineRenderer.endWidth = 0.1f;
+
+        //Show the border line
+        lineRenderer.enabled = true;
+    }
+
+    //Update is called one per frame
+     void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWWorldPoint(Input.mousePosition), Vector2.zero);
+            if (hit)
+            {
+                //Everything is moveable, so we don't need to check it's a Piece
+                draggingPiece = hit.transform;
+                offset = draggingPiece.position - Camera.main.ScreenToWorldPoint(Input.mousePosition;
+                offset += Vector3.back;
+           }
+       }
+        // When we release the mouse button stop dragging.
+        if (draggingPiece && Input.GetMouseButtonUp(0))
+        {
+            SnapAndDisableIfCorrect();
+            draggingPiece.position += Vector3.forward;
+            draggingPiece = null;
+        }
+
+        // Set the dragged piece position to the position of the mouse.
+        if (draggingPiece)
+        {
+            Vector3 newPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            //newPosition.z = draggingPiece.position.z;
+            newPosition += offset;
+            draggingPiece.position = newPosition;
+        }
+    }
+
+    private void SnapAndDisableIfCorrect()
+    {
+        // We need to know the index of the piece to determine it's correct position.
+        int pieceIndex = pieces.IndexOf(draggingPiece);
+
+        // The coordinates of the piece in the puzzle.
+        int col = pieceIndex % dimensions.x;
+        int row = pieceIndex / dimensions.x;
+
+        // The target position in the non-scaled coordinates.
+        Vector2 targetPosition = new((-width * dimensions.x / 2) + (width * col) + (width / 2),
+                                     (-height * dimensions.y / 2) + (height * row) + (height / 2));
+
+        // Check if we're in the correct location.
+        if (Vector2.Distance(draggingPiece.localPosition, targetPosition) < (width / 2))
+        {
+            // Snap to our destination.
+            draggingPiece.localPosition = targetPosition;
+
+            // Disable the collider so we can't click on the object anymore.
+            draggingPiece.GetComponent<BoxCollider2D>().enabled = false;
+
+            // Increase the number of correct pieces, and check for puzzle completion.
+            piecesCorrect++;
+            if (piecesCorrect == pieces.Count)
+            {
+                dialog.ResetDialog(); // *
+                dialog.gameObject.SetActive(true); // *
+
+                if (puzzleIndex == 0)
+                {
+                    dialog.dialogLines = new string[] {
+        "Đây là Laika – chú chó đầu tiên được Liên Xô đưa vào quỹ đạo Trái Đất năm 1957.",
+        "Mặc dù Laika không thể sống sót trở về, nhưng sứ mệnh đã mở đường cho các chuyến bay có người lái sau này.",
+        "Câu chuyện của Laika nhắc nhở chúng ta về những hy sinh trong buổi đầu thám hiểm vũ trụ."
+    };
+                    dialog.StartDialog();
+                }
+                else if (puzzleIndex == 1)
+                {
+                    dialog.dialogLines = new string[] {
+        "Đây là 'astronaut ice cream' – loại kem khô được phát triển cho NASA vào thập niên 1960.",
+        "Thực ra phi hành gia ngoài vũ trụ hiếm khi ăn món này, vì nó dễ vụn ra và gây nguy hiểm trong môi trường không trọng lực.",
+        "Ngày nay, nó chủ yếu được bán trong bảo tàng và cửa hàng lưu niệm như một kỷ niệm độc đáo."
+    };
+                    dialog.StartDialog();
+                }
+                else if (puzzleIndex == 2)
+                {
+                    dialog.dialogLines = new string[] {
+        "Đây là bộ đồ phi hành gia thế hệ mới – được NASA thiết kế cho chương trình Artemis.",
+        "So với những bộ cũ, nó linh hoạt hơn, nhẹ hơn và giúp phi hành gia dễ dàng cúi, ngồi và di chuyển.",
+        "Bộ đồ này còn có khả năng chống bụi Mặt Trăng tốt hơn và được tối ưu cho các sứ mệnh tới sao Hỏa trong tương lai."
+    };
+                    dialog.StartDialog();
+                }
+                else if (puzzleIndex == 3)
+                {
+                    dialog.dialogLines = new string[] {
+        "Đây là hình ảnh bề mặt sao Hỏa – hành tinh đỏ trong Hệ Mặt Trời.",
+        "Nhiều tàu thăm dò như Perseverance Rover đang tìm dấu hiệu của sự sống cổ xưa trên hành tinh này.",
+        "Sao Hỏa cũng là mục tiêu chính cho các sứ mệnh có người lái trong vài thập kỷ tới."
+    };
+                    dialog.StartDialog();
+                }
+                else if (puzzleIndex == 4)
+                {
+                    dialog.dialogLines = new string[] {
+        "Đây là ảnh tàn dư siêu tân tinh Cassiopeia A – chụp bởi Kính viễn vọng Chandra X-ray.",
+        "Cassiopeia A là kết quả của một ngôi sao khổng lồ nổ tung cách Trái Đất khoảng 11.000 năm ánh sáng.",
+        "Nghiên cứu siêu tân tinh giúp các nhà khoa học hiểu cách các nguyên tố nặng được tạo ra trong vũ trụ."
+    };
+                    dialog.StartDialog();
+                }
+                else if (puzzleIndex == 5)
+                {
+                    dialog.dialogLines = new string[] {
+        "Đây là hình minh họa hố đen vũ trụ – một trong những vật thể bí ẩn nhất.",
+        "Trường hấp dẫn của hố đen mạnh đến mức ngay cả ánh sáng cũng không thể thoát ra.",
+        "Hố đen đóng vai trò quan trọng trong việc định hình thiên hà và tạo ra những hiện tượng năng lượng cực lớn."
+    };
+                    dialog.StartDialog();
+                }
+                else if (puzzleIndex == 6)
+                {
+                    dialog.dialogLines = new string[] {
+        "Đây là Trạm Vũ trụ Quốc tế (ISS) – dự án hợp tác của nhiều quốc gia, hoạt động từ năm 2000.",
+        "ISS bay quanh Trái Đất khoảng 16 lần mỗi ngày, cho phép các phi hành gia nghiên cứu trong môi trường vi trọng lực.",
+        "Đây cũng là phòng thí nghiệm không gian lớn nhất từng được xây dựng, biểu tượng cho sự hợp tác quốc tế."
+    };
+                    dialog.StartDialog();
+                }
+
+
+            }
+        }
+    }
+
+    public void RestartGame()
+    {
+        Debug.Log(">>> [RestartGame] Bắt đầu");
+
+        // Destroy all the puzzle pieces.
+        foreach (Transform piece in pieces)
+        {
+            Destroy(piece.gameObject);
+        }
+        pieces.Clear();
+        // Hide the outline
+        gameHolder.GetComponent<LineRenderer>().enabled = false;
+        // Show the level select UI.
+        playAgainButton.SetActive(false);
+        levelSelectPanel.gameObject.SetActive(true);
+    }
 }
+
