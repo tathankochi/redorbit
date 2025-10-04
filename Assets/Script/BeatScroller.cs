d03903f71e06c24cc2b0dfa7d5bb9121274beb09
Kien-Truc Tran <kientruc0711@gmail.com>
2025-10-04 17:23:41 +0700

    Hah yessir

--- Assets/Script/BeatScroller.cs
+++ Assets/Script/BeatScroller.cs
@@ -13,16 +13,14 @@ public class BeatScroller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (!hasStarted)
-        {
-            if (Input.anyKeyDown)
-            {
-                hasStarted = true;
-            }
-        }
-        else
+        if (hasStarted)
         {
             transform.position -= new Vector3(0f, beatTempo * Time.deltaTime, 0f);
         }
+    }
+
+    public void start()
+    {
+        hasStarted = true;
     }
 }
