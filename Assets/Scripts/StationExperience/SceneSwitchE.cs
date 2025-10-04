e4a7e7b01c2efe3f58bb1a689c039b444c571ddd
tathankochi <75437017+tathankochi@users.noreply.github.com>
2025-10-04 22:14:15 +0700

    1013

--- Assets/Scripts/StationExperience/SceneSwitchE.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class SceneSwitchE : MonoBehaviour
-{
-    public void SwitchScene(string sceneName)
-    {
-        UnityEngine.SceneManagement.SceneManager.LoadScene(sceneName);
-    }
-}
