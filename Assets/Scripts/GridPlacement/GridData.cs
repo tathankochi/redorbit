f0dc9dd49d3a09a91ffc16dbc002190b6a889ac2
lehoangan02 <146849112+lehoangan02@users.noreply.github.com>
2025-10-04 10:41:08 +0700

    Add InputManager and enhance grid placement logic
    Introduced InputManager script for handling user input and map selection, and updated GridData to support multiple tilemaps and improved position validation. Updated GridPlacement scene to include InputManager and new components, and added a 'Grid' layer in project settings.

--- Assets/Scripts/GridPlacement/GridData.cs
+++ Assets/Scripts/GridPlacement/GridData.cs
@@ -3,27 +3,77 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.Tilemaps;
+using UnityEngine.Rendering.UI;
 
 public class GridData : MonoBehaviour
 {
     Dictionary<Vector3Int, PlacementData> placedObjects = new();
-    [SerializeField] Tilemap tilemap;
+    [SerializeField]
     private List<Tilemap> moduleTilemaps;
     public void AddObjectAt(Vector3Int gridPosition, Vector2Int objectSize,
         int ID, int placeObjectIndex)
     {
         List<Vector3Int> positionToOccupy = CalculatePositions(gridPosition, objectSize);
         PlacementData newData = new PlacementData(ID, placeObjectIndex, positionToOccupy);
+        foreach (var pos in positionToOccupy)
+        {
+            if (placedObjects.ContainsKey(pos))
+            {
+                Debug.LogError("Position " + pos + " is already occupied.");
+            }
+            placedObjects[pos] = newData;
+        }
     }
 
     private List<Vector3Int> CalculatePositions(Vector3Int gridPosition, Vector2Int objectSize)
     {
-        throw new NotImplementedException();
+        List<Vector3Int> positions = new List<Vector3Int>();
+        for (int x = 0; x < objectSize.x; ++x)
+        {
+            for (int y = 0; y < objectSize.y; ++y)
+            {
+                positions.Add(new Vector3Int(gridPosition.x + x, gridPosition.y + y, gridPosition.z));
+            }
+        }
+        return positions;
+    }
+    public bool IsPositionOnTilemap(List<Vector3Int> gridPositions)
+    {
+        foreach (var pos in gridPositions)
+        {
+            foreach (var tilemap in moduleTilemaps)
+            {
+                Vector3 world = tilemap.layoutGrid.CellToWorld(pos);
+                Vector3Int localCell = tilemap.WorldToCell(world);
+                if (tilemap != null && tilemap.HasTile(localCell) == true)
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    public bool IsPositionOccupied(Vector3Int gridPosition, Vector2Int objectSize)
+    {
+        // if the position is not on the tilemap, return true
+        List<Vector3Int> positionsToCheck = CalculatePositions(gridPosition, objectSize);
+        foreach (var pos in positionsToCheck)
+        {
+            if (placedObjects.ContainsKey(pos))
+            {
+                return true;
+            }
+        }
+        if (!IsPositionOnTilemap(positionsToCheck)) return true;
+        return false;
     }
 
     void Start()
     {
-
+        if (moduleTilemaps.Count == 0)
+        {
+            Debug.LogError("Module Tilemaps are not assigned.");
+        }
     }
 
     // Update is called once per frame
