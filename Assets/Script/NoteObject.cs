84d568c22f3a0f943df74451efe7bee85e4eaf96
Kien-Truc Tran <kientruc0711@gmail.com>
2025-10-04 10:43:43 +0700

    Bruv bruv bruv

---
+++ Assets/Script/NoteObject.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class NoteObject : MonoBehaviour
+{
+    public bool canBePressed;
+    public KeyCode keyToPress;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(keyToPress))
+        {
+            if (canBePressed)
+            {
+                gameObject.SetActive(false);
+            }
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Activator")
+        {
+            canBePressed = true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Activator")
+        {
+            canBePressed = false;
+        }
+    }
+}
