674ecf655b82d4e8f3235ed71d3c337ed73fad1c
penhilovelybaby <tranleyennhi1905@gmail.com>
2025-10-04 13:58:40 +0700

    hihi

---
+++ Assets/Script/JsonFileIO_COUNT.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using System.IO;
+
+public class JsonFileIO_COUNT : MonoBehaviour
+{
+    [System.Serializable]
+    private class CountData
+    {
+        public int count;
+    }
+
+    public static int ReadCountJson(string filename)
+    {
+        try
+        {
+            if (File.Exists(filename))
+            {
+                string json = File.ReadAllText(filename);
+                CountData data = JsonUtility.FromJson<CountData>(json);
+                Debug.Log($"Read {filename}");
+                return (data.count);
+            }
+        }
+        catch
+        {
+            // Trả về 0 nếu file không tồn tại hoặc lỗi
+        }
+        return (0);
+    }
+
+    public static void WriteCountJson(string filename, int count)
+    {
+        CountData data = new CountData {count = count};
+        string json = JsonUtility.ToJson(data, true);
+        File.WriteAllText(filename, json);
+        Debug.Log($"Write {filename}");
+    }
+}
