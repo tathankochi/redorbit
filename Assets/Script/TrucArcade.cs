b99098325060740c5b2be0127a90d3946336a4a7
Kien-Truc Tran <kientruc0711@gmail.com>
2025-10-04 10:51:37 +0700

    Blob blob like a fish

--- Assets/Script/TrucArcade.cs
+++ Assets/Script/TrucArcade.cs
@@ -2,6 +2,9 @@ using UnityEngine;
 
 public class TrucArcade : MonoBehaviour
 {
+    public AudioSource theMusic;
+    public bool startPlaying;
+    public BeatScroller theBS;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +14,15 @@ public class TrucArcade : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (!startPlaying)
+        {
+            if (Input.anyKeyDown)
+            {
+                startPlaying = true;
+                theBS.hasStarted = true;
+
+                theMusic.Play();
+            }
+        }
     }
 }
