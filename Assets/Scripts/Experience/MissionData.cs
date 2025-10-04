c050d0ca0a8afbc18062f149ee040f327605858b
lehoangan02 <146849112+lehoangan02@users.noreply.github.com>
2025-10-04 22:24:18 +0700

    Fix

--- Assets/Scripts/Experience/MissionData.cs
+++
@@ -1,9 +0,0 @@
-using System;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "MissionData", menuName = "Scriptable Objects/MissionData")]
-public class MissionData : ScriptableObject
-{
-    public List<String> missionDescriptions;
-}
