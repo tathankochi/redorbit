fc414d2cbb597b69f9c7012b1fad99cca9632e1b
Kien-Truc Tran <kientruc0711@gmail.com>
2025-10-05 02:11:31 +0700

    Bruhh

--- Assets/Script/TrucArcade.cs
+++ Assets/Script/TrucArcade.cs
@@ -5,32 +5,51 @@ public class TrucArcade : MonoBehaviour
     public AudioSource theMusic;
     public bool startPlaying;
     public BeatScroller theBS;
-    public static TrucArcade instance;
-    public Dialog dialog;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
     public void StartGame()
     {
         if (!startPlaying)
         {
+
             startPlaying = true;
             theBS.hasStarted = true;
 
-            if (theMusic != null)
-            {
-                theMusic.Play();
-            }
-            else
-            {
-                Debug.LogError("theMusic is not assigned in the Inspector!");
-            }
+            theMusic.Play();
         }
     }
-
-
-
-    void OnButtonClicked()
+    public Dialog dialog;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
     {
-        gameObject.SetActive(false);
+        Debug.Log("You are now exercising...");
+        Dialog dialog = FindDialogInScene();
+        if (dialog == null)
+        {
+            Debug.LogError("Dialog component not found in the scene.");
+            return;
+        }
+        if (dialog.gameObject.activeSelf) return;
+        dialog.ResetDialog();
+        dialog.gameObject.SetActive(true);
+        dialog.dialogLines = new string[] {
+            "The game is controlled by Up/Down/Right/Left arrows",
+            "Time it perfectly for the arrow to match the button and press it",
+            "Have fun!",
+            "Press SPACE to continue ..."
+        };
+        dialog.StartDialog();
+    }
+    private Dialog FindDialogInScene()
+    {
+        Dialog[] dialogs = Resources.FindObjectsOfTypeAll<Dialog>();
+        foreach (Dialog dialog in dialogs)
+        {
+            if (dialog.gameObject.scene.IsValid()) // Ensure it's part of the active scene
+            {
+                return dialog;
+            }
+        }
+        return null;
     }
 
     // Update is called once per frame
@@ -38,7 +57,6 @@ public class TrucArcade : MonoBehaviour
     {
 
     }
-
     public void NoteHit()
     {
         Debug.Log("Hit On Time!");
